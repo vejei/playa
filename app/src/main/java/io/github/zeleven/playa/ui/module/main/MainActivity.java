592e88c020c397eb875b435276feddf91d73a6dc
zeleven <chengsnem@163.com>
2018-08-30 21:34:39 +0800

    创建项目

---
+++ app/src/main/java/io/github/zeleven/playa/ui/module/main/MainActivity.java
@@ -0,0 +1,15 @@
+package io.github.zeleven.playa.ui.module.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.zeleven.playa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
