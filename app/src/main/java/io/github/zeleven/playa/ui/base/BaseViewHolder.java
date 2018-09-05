46f83912daa7b8a2ad3cc77b0b38df7038afc3d7
zeleven <chengsnem@163.com>
2018-09-05 20:24:44 +0800

    BaseViewHolder

---
+++ app/src/main/java/io/github/zeleven/playa/ui/base/BaseViewHolder.java
@@ -0,0 +1,17 @@
+package io.github.zeleven.playa.ui.base;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import butterknife.ButterKnife;
+
+public class BaseViewHolder<D> extends RecyclerView.ViewHolder {
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bind(D data) {}
+
+    public void bind(D data, int position) {}
+}
