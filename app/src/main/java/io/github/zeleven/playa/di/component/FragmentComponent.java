64b3ee30294d5570157fad60f47dd31f9a2b44ae
zeleven <chengsnem@163.com>
2018-09-05 20:22:45 +0800

    update

--- app/src/main/java/io/github/zeleven/playa/di/component/FragmentComponent.java
+++ app/src/main/java/io/github/zeleven/playa/di/component/FragmentComponent.java
@@ -3,8 +3,14 @@ package io.github.zeleven.playa.di.component;
 import dagger.Component;
 import io.github.zeleven.playa.di.module.FragmentModule;
 import io.github.zeleven.playa.di.scope.FragmentScope;
+import io.github.zeleven.playa.ui.module.main.home.HomeFragment;
+import io.github.zeleven.playa.ui.module.main.project.ProjectFragment;
+import io.github.zeleven.playa.ui.module.main.project.tabpage.ProjectTabPageFragment;
 
 @FragmentScope
 @Component(modules = FragmentModule.class, dependencies = ApplicationComponent.class)
 public interface FragmentComponent {
+    void inject(HomeFragment homeFragment);
+    void inject(ProjectFragment projectFragment);
+    void inject(ProjectTabPageFragment projectTabPageFragment);
 }
