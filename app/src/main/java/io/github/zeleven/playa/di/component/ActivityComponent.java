a56dfae88784d9d54a6e4eb5abea0dea5a947669
zeleven <chengsnem@163.com>
2018-09-05 20:21:51 +0800

    Add inject method

--- app/src/main/java/io/github/zeleven/playa/di/component/ActivityComponent.java
+++ app/src/main/java/io/github/zeleven/playa/di/component/ActivityComponent.java
@@ -3,8 +3,10 @@ package io.github.zeleven.playa.di.component;
 import dagger.Component;
 import io.github.zeleven.playa.di.module.ActivityModule;
 import io.github.zeleven.playa.di.scope.ActivityScope;
+import io.github.zeleven.playa.ui.module.main.MainActivity;
 
 @ActivityScope
 @Component(modules = ActivityModule.class, dependencies = ApplicationComponent.class)
 public interface ActivityComponent {
+    void inject(MainActivity mainActivity);
 }
