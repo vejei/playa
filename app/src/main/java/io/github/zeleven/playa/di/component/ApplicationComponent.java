d6856cea928e750b6de0cde42df081e7f4edf798
zeleven <chengsnem@163.com>
2018-09-05 20:22:17 +0800

    update

--- app/src/main/java/io/github/zeleven/playa/di/component/ApplicationComponent.java
+++ app/src/main/java/io/github/zeleven/playa/di/component/ApplicationComponent.java
@@ -5,10 +5,12 @@ import android.app.Application;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import io.github.zeleven.playa.data.source.DataManager;
 import io.github.zeleven.playa.di.module.ApplicationModule;
 
 @Singleton
 @Component(modules = ApplicationModule.class)
 public interface ApplicationComponent {
     Application application();
+    DataManager dataManager();
 }
