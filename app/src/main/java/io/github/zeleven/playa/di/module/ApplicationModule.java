52ef9756f1918101dc630984855d6df01cf5015b
zeleven <chengsnem@163.com>
2018-09-05 20:23:24 +0800

    Add provide method

--- app/src/main/java/io/github/zeleven/playa/di/module/ApplicationModule.java
+++ app/src/main/java/io/github/zeleven/playa/di/module/ApplicationModule.java
@@ -7,6 +7,11 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import io.github.zeleven.playa.Playa;
+import io.github.zeleven.playa.data.source.remote.WanAndroidService;
+import io.github.zeleven.playa.utils.Constants;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class ApplicationModule {
@@ -20,5 +25,21 @@ public class ApplicationModule {
     @Singleton
     Application provideApplication() {
         return application;
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+        return retrofit;
+    }
+
+    @Provides
+    @Singleton
+    WanAndroidService provideWanAndroidService(Retrofit retrofit) {
+        return retrofit.create(WanAndroidService.class);
     }
 }
