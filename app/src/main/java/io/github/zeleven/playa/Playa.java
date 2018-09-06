d99a1871db392c8035f6ccb2b2f502f423dd812e
zeleven <chengsnem@163.com>
2018-09-06 14:15:21 +0800

    Realm 数据库初始化

--- app/src/main/java/io/github/zeleven/playa/Playa.java
+++ app/src/main/java/io/github/zeleven/playa/Playa.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import io.github.zeleven.playa.di.component.ApplicationComponent;
 import io.github.zeleven.playa.di.component.DaggerApplicationComponent;
 import io.github.zeleven.playa.di.module.ApplicationModule;
+import io.realm.Realm;
 
 public class Playa extends Application {
     private ApplicationComponent applicationComponent;
@@ -15,6 +16,7 @@ public class Playa extends Application {
         applicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this))
                 .build();
+        Realm.init(this);
     }
 
     public ApplicationComponent getApplicationComponent() {
