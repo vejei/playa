962b4de66edb7c860da5c21de651713a80e9b09a
zeleven <chengsnem@163.com>
2018-09-06 14:18:48 +0800

    tab viewpager 工具类

---
+++ app/src/main/java/io/github/zeleven/playa/utils/TabViewPagerAdapterItem.java
@@ -0,0 +1,38 @@
+package io.github.zeleven.playa.utils;
+
+import android.support.v4.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.zeleven.playa.data.model.Category;
+import io.github.zeleven.playa.ui.module.main.project.tabpage.ProjectTabPageFragment;
+
+public class TabViewPagerAdapterItem {
+    private String title;
+    private Fragment fragment;
+
+    public TabViewPagerAdapterItem(String title, Fragment fragment) {
+        this.title = title;
+        this.fragment = fragment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Fragment getFragment() {
+        return fragment;
+    }
+
+    public static List<TabViewPagerAdapterItem> createProjectTabFragments(List<Category> categories) {
+        ArrayList<TabViewPagerAdapterItem> adapterItems = new ArrayList<>();
+        for (int i = 0; i < categories.size(); i++) {
+            adapterItems.add(
+                    new TabViewPagerAdapterItem(categories.get(i).getName(),
+                    ProjectTabPageFragment.newInstance(categories.get(i).getId()))
+            );
+        }
+        return adapterItems;
+    }
+}
